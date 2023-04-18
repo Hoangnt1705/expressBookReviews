ee91be5ab856c098f3b6d90352ffe4a55757b437
Hoangnt1705 <tuanhoang1705@gmail.com>
2023-04-18 05:25:17 -0400

    18/4/2023

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
 const app = express();
 
 app.use(express.json());
@@ -32,5 +31,4 @@ const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
